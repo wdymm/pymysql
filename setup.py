d9e773de7b4027229693cc829c6634a606c841e5
wdymm <49129911+wdymm@users.noreply.github.com>
2019-09-20 20:47:18 -0500

    copy

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import io
+from setuptools import setup, find_packages
+
+version = "0.9.3"
+
+with io.open('./README.rst', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name="PyMySQL",
+    version=version,
+    url='https://github.com/PyMySQL/PyMySQL/',
+    project_urls={
+        "Documentation": "https://pymysql.readthedocs.io/",
+    },
+    description='Pure Python MySQL Driver',
+    long_description=readme,
+    packages=find_packages(exclude=['tests*', 'pymysql.tests*']),
+    extras_require={
+        "rsa": ["cryptography"],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Database',
+    ],
+    keywords="MySQL",
+)
